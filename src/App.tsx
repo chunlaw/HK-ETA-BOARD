997cc271d5596a184431cac77d3880abdb51c104
Law Wai Chun <chunlaw@rocketmail.com>
2023-08-27 19:16:24 +0800

    Update README.md

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Content from "./layouts/Content";
 import { Container, SxProps, Theme } from "@mui/material";
 import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
     <Container maxWidth="md" fixed sx={rootSx}>
       <Header />
       <Content />
+      <Footer />
     </Container>
   );
 }
